712d393350342c5e21e406a17d8173dff00cecf8
Marco <58081927+marco-bolarinho@users.noreply.github.com>
2024-06-10 17:29:46 +0000

    Create exec.sh

---
+++ rproxy/exec.sh
@@ -0,0 +1,7 @@
+kubectl delete configmap/nginx-config
+kubectl delete deployment/nginx-proxy
+kubectl delete service/nginx-service
+
+kubectl create configmap nginx-config --from-file=nginx.conf
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
