c40ec3fa34068ef93c984727d27fe6f53be75a86
marco bolarinho <marcoandrebolarinho@enta.pt>
2024-06-13 09:01:08 +0000

    testing

--- setup/01-reverse-proxy/run.sh
+++ setup/01-reverse-proxy/run.sh
@@ -1,8 +1,10 @@
 kubectl delete configmap/nginx-config
+kubectl delete configmap/index
 kubectl delete deployment/nginx-proxy
 kubectl delete service/nginx-service
 
 kubectl create configmap nginx-config --from-file=nginx.conf
+kubectl create configmap index --from-file=index.html
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
 
