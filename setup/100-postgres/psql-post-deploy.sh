1458940249444ea4c05f1838d5c31b639dbdd53a
marco bolarinho <marcoandrebolarinho@enta.pt>
2024-06-14 11:39:48 +0000

    testing

--- setup/100-postgres/psql-post-deploy.sh
+++ setup/100-postgres/psql-post-deploy.sh
@@ -8,5 +8,19 @@ kubectl exec -it service/postgres-service -- psql postgresql://admin:Passw0rd@lo
   CREATE DATABASE plik_db;
   GRANT ALL PRIVILEGES ON DATABASE plik_db TO plik;
 
+  CREATE USER ots;
+  ALTER USER ots WITH PASSWORD 'Passw0rd';
+  CREATE DATABASE ots_db;
+  GRANT ALL PRIVILEGES ON DATABASE ots_db TO ots;
+
+  CREATE USER ejbca;
+  ALTER USER ejbca WITH PASSWORD 'Passw0rd';
+  CREATE DATABASE ejbca_db;
+  GRANT ALL PRIVILEGES ON DATABASE ejbca TO ejbca;
+
+  CREATE USER wiki;
+  ALTER USER wiki WITH PASSWORD 'Passw0rd';
+  CREATE DATABASE wiki_db;
+  GRANT ALL PRIVILEGES ON DATABASE wiki_db TO wiki;
 
 EOSQL
