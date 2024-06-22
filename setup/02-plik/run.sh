9c3b80e61505b91cf2d39ba1607b54696084ccfc
Marco Bolarinho <marcoandrebolarinho@enta.pt>
2024-06-22 18:36:30 +0000

    enabled plik persistency with postgres and efs

--- setup/02-plik/run.sh
+++ setup/02-plik/run.sh
@@ -1,2 +1,8 @@
+kubectl delete configmap/plik-cfg
+kubectl delete deployment/plik
+kubectl delete service/plik-service
+
+kubectl apply -f pvc.yaml
+kubectl create configmap plik-cfg --from-file=plikd.cfg
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
--- setup/103-postgres/run.sh
+++ setup/103-postgres/run.sh
@@ -1,8 +1,9 @@
 kubectl delete deployment/postgres
 kubectl delete service/postgres-service
-#kubectl delete pvc/postgres-pvc
+kubectl delete pvc/postgres-pvc
+#kubectl delete configmap/postgres-initdb
 
-kubectl apply -f init-configmap.yaml
+#kubectl apply -f init-configmap.yaml
 kubectl apply -f pvc.yaml
 kubectl apply -f secrets.yaml
 kubectl apply -f deployment.yaml
